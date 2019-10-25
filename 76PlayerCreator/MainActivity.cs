74dc3375ef61605986ff09dfad429cc48fdf19d3
mellyonz <sidder19@hotmail.com>
2019-10-26 12:50:51 +1300

    Initial commit
    Json storage and Navigation tabs have been added

---
+++ 76PlayerCreator/MainActivity.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Android;
+using Android.App;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using SpecialDefualt;
+
+namespace _76PlayerCreator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        TextView textMessage;
+        Special[] special;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawer.AddDrawerListener(toggle);
+            toggle.SyncState();
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+
+            textMessage = FindViewById<TextView>(Resource.Id.message);
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+
+            navigation.SetOnNavigationItemSelectedListener(this);
+
+            AssetManager assets = this.Assets;
+            using (StreamReader sr = new StreamReader(assets.Open("special.json")))
+            {
+                var json = sr.ReadToEnd();
+                var rootobject = JsonConvert.DeserializeObject<Rootobject>(json);
+                special = rootobject.special;
+            }
+            Console.WriteLine(special);
+
+        }
+
+        public override void OnBackPressed()
+        {
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            if(drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            Android.Views.View view = (Android.Views.View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.nav_camera)
+            {
+                // Handle the camera action
+            }
+            else if (id == Resource.Id.nav_gallery)
+            {
+
+            }
+            else if (id == Resource.Id.nav_slideshow)
+            {
+
+            }
+            else if (id == Resource.Id.nav_manage)
+            {
+
+            }
+            else if (id == Resource.Id.nav_share)
+            {
+
+            }
+            else if (id == Resource.Id.nav_send)
+            {
+
+            }
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    using (var writer = new System.IO.StringWriter())
+                    {
+                        string json = JsonConvert.SerializeObject(special, Formatting.Indented);
+                        Console.WriteLine(json);
+                        Console.WriteLine(special[1]);
+                        Console.WriteLine(special);
+                        textMessage.Text = special[1].ToString();
+                    }
+                    return true;
+                case Resource.Id.navigation_dashboard:
+                    textMessage.SetText(Resource.String.title_dashboard);
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    textMessage.SetText(Resource.String.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ 76PlayerCreator/Special.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SpecialDefualt
+{
+    public class Special
+    {
+        public string mainColor { get; set; }
+        public float maxPoints { get; set; }
+        public float assignedPoints { get; set; }
+        public float totPointsUsedByCards { get; set; }
+        public float branch { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+
+
+        public override string ToString()
+        {
+            return string.Format(name);
+        }
+    }
+
+    public class Rootobject
+    {
+        public Special[] special { get; set; }
+    }
+}
